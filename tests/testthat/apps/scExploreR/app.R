735fb6070e89cded52ef329f5590e4c51bbb6cb8
Bill Showers <william.showers@ucdenver.edu>
2023-02-23 15:59:28 -0700

    Automated test for DGE marker identification
    Created automated testing framework via testthat and shinytest to verify marker identification in the DGE tab runs correctly.

--- tests/testthat/apps/scExploreR/app.R
+++ tests/testthat/apps/scExploreR/app.R
@@ -1,6 +1,7 @@
 library(scExploreR)
 
 run_scExploreR(
-  object_path = system.file("extdata", "test_dataset.rds", package = "SCEPlots"),
-  config_path = system.file("extdata", "test_dataset_conig.yaml", package = "SCEPlots")
+  object_path = system.file("extdata", "test_dataset.rds", package = "scExploreR", mustWork = TRUE),
+  config_path = system.file("extdata", "test_dataset_config.yaml", package = "scExploreR", mustWork = TRUE),
+  launch.browser = TRUE
 )
--- tests/testthat/test-app_test.R
+++ tests/testthat/test-app_test.R
@@ -1,3 +1,28 @@
-test_that("no errors", {
+library(shinytest)
+library(shiny)
+
+app <- ShinyDriver$new("./apps/scExploreR", seed = 325)
+
+test_that("No errors for Marker ID", {
+  # Swtich to DGE tab: find button in navbar panel, and click the button 
+  dge_tab_btn <- app$findElement(linkText = "Differential Expression")
+  dge_tab_btn$click()
   
+  # Must wait for the DGE tab submit button to appear 
+  app$waitForShiny()
+  
+  # No adjustments necessary to run marker identification; just click submit
+  app$click("object_dge-submit")
+  # Wait for table to compute after pressing submit
+  app$waitForShiny()
+  
+  # The output below will appear only if the DGE test ran without errors
+  expect_true(isTruthy(app$getValue("object_dge-subset_stats-n_by_class")))
+  # The app should also be able to screenshot the table without issue
+  expect_no_error(
+    app$takeScreenshot(
+      id = "object_dge-table", 
+      file = NULL
+      )
+    )
 })
