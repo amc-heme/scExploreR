53687bfa3d75e34c1637881a02f3ce637c8652d9
Bill Showers <william.showers@ucdenver.edu>
2023-05-24 17:51:49 -0600

    Update documentation for n_unique

--- R/n_unique.R
+++ R/n_unique.R
@@ -1,7 +1,8 @@
-#' Number of Classes in Seurat Object
+#' Object metadata: number of unique values
 #'
-#' Returns the number of unique values in the Seurat object, for the specified
-#' metadata column.
+#' Returns the number of unique values in a single cell object, for the specified
+#' metadata variable. Any object type supported by 
+#' \code{\link[SCEPlots::unique_values]{SCEPlots::unique_values}} may be used.
 #'
 #' @param object A Seurat object.
 #' @param meta_var Name of a metadata variable.
