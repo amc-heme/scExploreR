df416b9cca1f6f83e1bde5dd4e9d1eb3b0c16b62
jairav <jairav@gmail.com>
2024-09-19 13:22:11 -0400

    tossing all the old tests

--- tests/testthat/apps/scExploreR/app.R
+++
@@ -1,7 +0,0 @@
-library(scExploreR)
-
-run_scExploreR(
-  object_path = system.file("extdata", "test_dataset.rds", package = "scExploreR", mustWork = TRUE),
-  config_path = system.file("extdata", "test_dataset_config.yaml", package = "scExploreR", mustWork = TRUE),
-  dev_mode = TRUE
-)
